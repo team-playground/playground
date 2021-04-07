3a52f473d55172651c840651dbfa5767aa087876
zkdlu <zkdlu159951@gmail.com>
2021-04-07 22:02:41 +0900

    user usecase

---
+++ module-application-user/src/main/java/com/playground/user/application/in/UserFindUseCase.java
@@ -0,0 +1,4 @@
+package com.playground.user.application.in;
+
+public interface UserFindUseCase {
+}
--- module-domain-user/src/main/java/com/playground/user/domain/User.java
+++ module-domain-user/src/main/java/com/playground/user/domain/User.java
@@ -1,5 +1,7 @@
 package com.playground.user.domain;
 
+import com.playground.team.domain.Team;
+import com.playground.team.domain.TeamMember;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,6 +22,27 @@ public class User {
         this.name = name;
         this.picture = picture;
         this.isPublic = true;
+    }
+
+    public Team makeTeam(String teamId, String banner, String icon, String teamName, String teamColor) {
+        return Team.builder()
+                .id(teamId).name(teamName)
+                .banner(banner).icon(icon).color(teamColor)
+                .owner(convertTeamMember())
+                .build();
+    }
+
+    public void joinTeam(Team team) {
+        team.join(convertTeamMember());
+    }
+
+    public TeamMember convertTeamMember() {
+        return TeamMember.builder()
+                .id(id)
+                .email(email)
+                .name(name)
+                .picture(picture)
+                .build();
     }
 
     public void visible() {
